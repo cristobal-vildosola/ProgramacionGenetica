b1e27fa23bf8e9491670150e1974e28db888fc26
Cristobal Muñoz <cristobal.vildosola@gmail.com>
2019-09-27 00:36:11 -0300

    refactor genetic algorithm to be easier to use, refactor word finder accordingly

---
+++ tarea2/GeneticAlgorithm.py
@@ -0,0 +1,156 @@
+import random
+from abc import ABC, abstractmethod
+from typing import Callable, List, Tuple
+
+import numpy
+import matplotlib.pyplot as plt
+
+
+class Gene(ABC):
+    @abstractmethod
+    def mutate(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class Individual:
+    def __init__(self, genes: List[Gene]):
+        self.genes: List[Gene] = genes
+
+    def crossover(self, other):
+        index = random.randint(0, len(self.genes) - 1)
+        new_params = self.genes[:index] + other.genes[index:]
+
+        index = random.randint(0, len(self.genes) - 1)
+        new_params[index] = new_params[index].mutate()
+
+        return Individual(new_params)
+
+    def __str__(self):
+        return "".join([str(x) for x in self.genes])
+
+
+class Population:
+    def __init__(self,
+                 genes: List[Gene],
+                 fitness: Callable,
+                 size: int = 100,
+                 tournament_size=5):
+
+        self.genes: List[Gene] = genes
+        self.fitness: Callable = fitness
+
+        self.size: int = size
+        self.tournament_size: int = tournament_size
+
+        self.individuals: List[Individual] = []
+        self.fitnesses: List[float] = numpy.zeros(size)
+
+        self.gen_individuals()
+
+    def gen_individuals(self):
+        self.individuals = []
+
+        for _ in range(self.size):
+            new_params = [x.mutate() for x in self.genes]
+            self.individuals.append(Individual(new_params))
+
+        return
+
+    def evolve(self):
+        # calculate fitness for all individuals
+        self.calc_fitnesses()
+
+        max_fit = max(self.fitnesses)
+        min_fit = min(self.fitnesses)
+        mean_fit = numpy.mean(self.fitnesses)
+        best_ind = self.individuals[int(numpy.argmax(self.fitnesses))]
+
+        # select 2*n individuals in tournaments
+        selected = []
+        for _ in range(self.size * 2):
+            selected.append(self.tournament())
+
+        # generate new individuals
+        new_individuals = []
+        for i in range(0, self.size * 2, 2):
+            new_individuals.append(selected[i].crossover(selected[i + 1]))
+
+        self.individuals = new_individuals
+        return min_fit, mean_fit, max_fit, str(best_ind)
+
+    def calc_fitnesses(self):
+        for i in range(self.size):
+            self.fitnesses[i] = self.fitness(self.individuals[i])
+        return
+
+    def tournament(self):
+        candidates = random.sample(range(self.size), k=self.tournament_size)
+
+        best = candidates[0]
+        for i in candidates:
+            if self.fitnesses[i] > self.fitnesses[best]:
+                best = i
+
+        return self.individuals[best]
+
+
+class GeneticAlgorith:
+    def __init__(self,
+                 genes: List[Gene],
+                 fitness: callable,
+                 size: int = 100,
+                 tournament_size: int = 5):
+
+        self.population = Population(
+            genes=genes,
+            fitness=fitness,
+            size=size,
+            tournament_size=tournament_size,
+        )
+
+        self.history = {
+            'mins': [],
+            'means': [],
+            'maxs': [],
+            'bests': [],
+        }
+
+    def evolve(self, iters=30, verbose: bool = True, end_condition: callable = None):
+        if iters < 1:
+            iters = 999999999
+
+        for it in range(iters):
+            min_f, mean_f, max_f, best_ind = self.population.evolve()
+
+            self.history['mins'].append(min_f)
+            self.history['means'].append(mean_f)
+            self.history['maxs'].append(max_f)
+            self.history['bests'].append(best_ind)
+
+            if verbose:
+                print(f'iteration {it:3}: {best_ind} ({max_f})')
+
+            if end_condition is not None and end_condition(self):
+                break
+
+        return
+
+    def plot_history(self, fitness_range: Tuple[int, int] = None):
+        plt.figure()
+
+        plt.plot(self.history['mins'], 'r-')
+        plt.plot(self.history['means'], 'b-')
+        plt.plot(self.history['maxs'], 'g-')
+        plt.ylim(fitness_range)
+
+        plt.title('Population Evolution')
+        plt.xlabel('iteration')
+        plt.ylabel('fitness')
+        plt.legend(['min', 'mean', 'max'])
+
+        plt.show()
+        return
---
+++ tarea2/WordFinder.py
@@ -0,0 +1,53 @@
+import random
+import string
+from typing import List, Union, Tuple
+
+from GeneticAlgorithm import Gene, GeneticAlgorith, Individual
+
+
+class Char(Gene):
+    def __init__(self, alphabet: Union[List[str], Tuple[str, ...]] = tuple(string.printable[:-5])):
+        self.alphabet = alphabet
+        self.char = random.choice(alphabet)
+
+    def mutate(self):
+        return Char(self.alphabet)
+
+    def __str__(self):
+        return self.char
+
+
+def guess_word(word='askdjhas', iters=30, size=100, tournament_size=5):
+    genes = [Char() for _ in range(len(word))]
+
+    def fitness(ind: Individual):
+        score = 0
+        for i in range(len(word)):
+            score += word[i] == str(ind.genes[i])
+
+        return score
+
+    def end_condition(gen_alg):
+        return gen_alg.history['maxs'][-1] == len(word)
+
+    genetic_alg = GeneticAlgorith(
+        genes=genes,
+        fitness=fitness,
+        size=size,
+        tournament_size=tournament_size
+    )
+
+    genetic_alg.evolve(iters=iters, verbose=True, end_condition=end_condition)
+    genetic_alg.plot_history()
+
+    return
+
+
+if __name__ == '__main__':
+    user_word = input('word to guess? ')
+    guess_word(
+        user_word,
+        iters=-1,
+        size=len(user_word) * 10,
+        tournament_size=max(len(user_word) // 2, 5),
+    )
